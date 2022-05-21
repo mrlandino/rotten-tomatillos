0c54fc98c886e6f8b2b2fe7e768fa3d8ae574078
Ross Landino <mrlandino@gmail.com>
2022-05-20 19:19:59 -0600

    Update and refactor Testing

--- cypress/integration/app-spec.js
+++ cypress/integration/app-spec.js
@@ -20,8 +20,16 @@ describe('App', () => {
     cy.get('main').children('div').contains('Braveheart').should('not.exist')
   })
 
+  it('should relocate to error screen if api call fails', () => {
+    cy.intercept('GET', 'https://rancid-tomatillos.herokuapp.com/api/v2/movies', 'localhost3000/error')
+    cy.url().should('include', '/error')
+    cy.get('main').find('h3').contains("Something went wrong, Please try again later.")
+
+  })
+
   it('should be able to click on a poster and be taken to that movies details page', () => {
     cy.get('.movie').first().click()
+    cy.url().should('include', '/694919')
     cy.intercept('GET', 'https://rancid-tomatillos.herokuapp.com/api/v2/movies/694919', {fixture: 'movie'}).as('movie')
   })
 })
@@ -30,6 +38,7 @@ describe('Movie Details', () => {
   beforeEach(() => {
     cy.visit('http://localhost:3000')
     cy.get('.movie').first().click()
+    cy.url().should('include', '/694919')
     cy.intercept('GET', 'https://rancid-tomatillos.herokuapp.com/api/v2/movies/694919', {fixture: 'movie'}).as('movie')
   })
 
@@ -45,29 +54,16 @@ describe('Movie Details', () => {
     cy.get('.overview').contains("A professional thief with $40 million in debt and his family's life on the line must commit one final heist - rob a futuristic airborne casino filled with the world's most dangerous criminals.")
     cy.get('.below-tag').contains('2020 | Action | 82 min | 6.9')
     cy.get('iframe').should('have.attr', 'src').should('include', 'http://www.youtube.com/embed/aETz_dRDEys')
+    // cy.get('.ytp-large-play-button').click()
+    // allow user to click on youTube video
   })
 
   it('should have a home button that brings you back to main page', () => {
-    cy.get('button').click()
+    cy.get('.home').click()
     cy.get('nav').find('h1').contains('Rotten')
     cy.get('nav').find('h2').contains('Tomatillos')
-    // cy.get('nav').find('h1').contains('Rotten')
-    // cy.get('nav').find('h2').contains('Tomatillos')
     cy.get('.movie').should('have.length', 40)
     cy.get('main').children('div').contains('Mulan')
     cy.get('button').should('not.exist');
   })
-
-  // it.only("should return an error message if api call fails", () => {
-  //   cy.intercept(
-  // {
-  //   method: 'GET',
-  //   url: 'https://rancid-tomatillos.herokuapp.com/api/v2/movies',
-  //   hostname: 'localhost'
-  // })
-  // cy.intercept("GET", 'https://rancid-tomatillos.herokuapp.com/api/v2/movies', {
-  //   statusCode: 404
-  // })
-  // cy.get('main').find('h3').contains("Something went wrong, Please try again later.")
-  // })
 })
--- src/apiCalls.js
+++ src/apiCalls.js
@@ -1,14 +1,14 @@
 const getMovies = () => {
-  return (
-    fetch('https://rancid-tomatillos.herokuapp.com/api/v2/movies')
-        .then(response => {
-        if(response.ok) {
-            return response.json();
-        } else {
-            throw new Error();
-        }
-    }) 
-  )
+    return (
+        fetch('https://rancid-tomatillos.herokuapp.com/api/v2/movies')
+            .then(response => {
+            if(response.ok) {
+                return response.json();
+            } else {
+                throw new Error();
+            }
+        }) 
+    )
 }
 
 const getMovieDetails = (movie) => {
